274b40a3827711d514cb25e4d37048378f12a921
apivich <aefvip@gmail.com>
2021-02-17 08:51:03 +0700

    navtest

--- manage_dataBase.php
+++ manage_dataBase.php
@@ -4,24 +4,21 @@ class Data_Base
 {
     private $hostname = "localhost";
     private $username = "root";
-    private $password = " ";
-    private $dbname = "db_lullaby"; 
+    private $password = "";
+    private $dbname = "db_lullaby";
 
     public function check_sql()
     {
         $mysqli = mysqli_connect($this->hostname, $this->username, $this->password, $this->dbname);
-        if ($mysqli -> connect_errno) {
-            // echo ("Failed to connect to MySQL:" . $mysqli -> connect_error);
-            echo ('<script>consol.log("Failed to connect to MySQL");</script>');
-            exit();
-          }
-          
-          // Perform a query, check for error
-          if (!$mysqli) {
-            echo("Error description: ");
-            echo ('<script>consol.log("t");</script>');
-          }
-          
-          $mysqli -> close();
+        if ($mysqli) {
+            echo ('<script>console.log("connect to MySQL 👍");</script>');
+        }
+
+        //error
+        if (!$mysqli) {
+            echo ('<script>console.log("Failed to connect to MySQL 😡");</script>');
+        }
+
+        mysqli_close($mysqli);
     }
 }
